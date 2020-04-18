18ed404f090ac5984177a67138b3e30441b5c409
Tung Xuan <tungxuan@Tungs-iMac-Pro.local>
2020-04-18 16:10:34 +0700

    First Time Commit, App is Finish

---
+++ app.js
@@ -0,0 +1,384 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (request, response) {
+    response.sendFile("index.html");
+});
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
+
+var io;
+io = require("socket.io").listen(4000);
+var clients = [];
+var roundNumber = 3;
+var timeRound = 120;
+var startTimeMatch = new Date().getTime() / 1000;
+var currentRound = 0;
+var currentIndexUser = 0;
+var currentQuestion = "";
+var allUserAnswered = false;
+var resultLastMatch = {};
+var isFinishGame = false;
+var inMatch = false;
+var questions = [];
+
+// socket.broadcast là gửi sự kiện đến cho tất cả socket khác trừ đối tượng socket
+// io.emit là gửi sự kiện đến tất cả socket đã kết nối
+// socket.emit
+
+io.sockets.on("connection", function (socket) {
+    console.log(socket.id);
+
+    socket.on("init", function (data) {
+        console.log("New Client:", data.name, socket.id);
+        var host = false;
+        if (clients.length == 0) {
+            host = true;
+            socket.emit("changeHost", {});
+        } else host = false;
+        clients.push({ id: socket.id, socket: socket, name: data.name, point: data.point, host: host });
+        console.log("Number of Clients:", clients.length);
+        updateUsers();
+        appendNotification("Người chơi mới: " + data.name);
+        if (clients.length == 1) {
+            loadData(
+                function () {
+                    appendNotification("Load data success");
+                },
+                function () {
+                    appendNotification("Load data error");
+                }
+            );
+        }
+    });
+
+    socket.on("startGame", function (data) {
+        if (clients.length < 2) {
+            socket.emit("alert", { message: "Chưa Đủ Người Chơi. Cần Tối Thiểu 3 Người Chơi!" });
+        } else {
+            isFinishGame = false;
+            clients.forEach((client) => {
+                client.point = 0;
+            });
+            socket.emit("hideHostControl");
+            console.log("start game", data);
+            roundNumber = data.round;
+            timeRound = data.timeRound;
+            currentRound = 0;
+            startRound();
+            updateUsers();
+        }
+    });
+
+    socket.on("comment", function (data) {
+        var text = data.text;
+        // filter answer
+        if (inMatch && text.toLowerCase() === currentQuestion) {
+            appendChatCorrectAnswer(data.name);
+            if (data.name != clients[currentIndexUser].name) {
+                var interval = new Date().getTime() - startTimeMatch;
+                resultLastMatch[data.name] = parseInt((timeRound - interval / 1000) * 5);
+                allUserAnswered = true;
+                var keys = Object.keys(resultLastMatch);
+                keys.forEach((key) => {
+                    if (resultLastMatch[key] == 0) {
+                        allUserAnswered = false;
+                    }
+                });
+            }
+        } else {
+            appendChat(data.name, text);
+        }
+    });
+
+    socket.on("drawClick", function (data) {
+        socket.broadcast.emit("draw", {
+            x: data.x,
+            y: data.y,
+            type: data.type,
+        });
+    });
+
+    socket.on("clear_all", function (data) {
+        socket.broadcast.emit("clear_all", {});
+    });
+
+    socket.on("changeColor", function (data) {
+        socket.broadcast.emit("changeColor", data);
+    });
+
+    socket.on("changeLineWidth", function (data) {
+        socket.broadcast.emit("changeLineWidth", data);
+    });
+
+    socket.on("disconnect", function (data) {
+        var name = "";
+        console.log(socket.id);
+        for (var i = clients.length - 1; i >= 0; i--) {
+            if (clients[i].id === socket.id) {
+                name = clients[i].name;
+                clients.splice(i, 1);
+                if (i == 0 && clients.length != 0) {
+                    clients[0].socket.emit("changeHost", {});
+                    clients[0].host = true;
+                }
+            }
+        }
+        console.log("Number of Clients:", clients.length);
+        if (clients.length <= 0) isFinishGame = true;
+        updateUsers();
+        appendNotification("Người chơi đã thoát: " + name);
+        delete resultLastMatch[name];
+    });
+
+    function updateUsers() {
+        var list_user = clients.map(function (x) {
+            return { name: x.name, point: x.point };
+        });
+        list_user.sort((a, b) => {
+            return a.point < b.point;
+        });
+        io.emit("updateUsers", list_user);
+    }
+
+    function appendChatCorrectAnswer(name) {
+        io.emit("appendChatCorrectAnswer", { name: name });
+    }
+
+    function appendChat(name, message) {
+        io.emit("appendChat", { name: name, message: message });
+    }
+
+    function appendNotification(message) {
+        io.emit("appendNotification", { message: message });
+    }
+
+    function appendNotificationPoint(message) {
+        io.emit("appendNotificationPoint", { message: message });
+    }
+
+    function startRound() {
+        if (currentRound > roundNumber) {
+            appendNotification("Đã Kết Thúc Game!");
+            reGrantDrawPermission(true);
+            if (clients.length > 0) {
+                clients[0].socket.emit("changeHost", {});
+            }
+            console.log("Finish Game");
+        } else {
+            var round = currentRound + 1;
+            appendNotification("Vòng " + round);
+            currentIndexUser = 0;
+            startMatch();
+        }
+    }
+
+    function controlMatch() {
+        if (isFinishGame === true) {
+            reGrantDrawPermission(true);
+            if (clients.length > 0) {
+                clients[0].socket.emit("changeHost", {});
+            }
+            appendNotification("Đã Kết Thúc Game!");
+            console.log("Finish Game");
+            return;
+        }
+        var isFinishMatch = checkMatch();
+        if (isFinishMatch === true) {
+            // append notification update point
+            var drawPoint = 0;
+            var keys = Object.keys(resultLastMatch);
+            keys.forEach((key) => {
+                drawPoint += resultLastMatch[key];
+            });
+            if (drawPoint == null || drawPoint == undefined) drawPoint = -1;
+            drawPoint = (drawPoint * 2) / keys.length;
+            resultLastMatch[clients[currentIndexUser].name] = drawPoint;
+            for (var i = 0; i < clients.length; i++) {
+                var point = resultLastMatch[clients[i].name];
+                if (point == null || point == undefined) point = 0;
+                clients[i].point += point;
+                appendNotificationPoint(clients[i].name + ": " + point + " điểm");
+            }
+            updateUsers();
+            // finish match
+            io.emit("appendNotification", { message: "Từ Khoá là: " + currentQuestion });
+            //
+            currentIndexUser += 1;
+            if (currentIndexUser >= clients.length) {
+                currentRound += 1;
+                startRound();
+            } else {
+                startMatch();
+            }
+        } else {
+            setTimeout(controlMatch, 1000);
+        }
+    }
+
+    function startMatch() {
+        inMatch = false;
+        currentQuestion = randomQuestion();
+        appendNotification("chuẩn bị...");
+        setTimeout(function () {
+            inMatch = true;
+            startTimeMatch = new Date().getTime();
+            reGrantDrawPermission(false);
+            sendQuestion();
+            console.log("start Match");
+            io.emit("startMatch", { timeRound: timeRound });
+            appendNotification(clients[currentIndexUser].name + " đang vẽ!");
+            resultLastMatch = {};
+            clients.forEach((client) => {
+                resultLastMatch[client.name] = 0;
+            });
+            resultLastMatch[clients[currentIndexUser].name] = 1;
+            setTimeout(controlMatch, 1000);
+        }, 3000);
+    }
+
+    function checkMatch() {
+        var now = new Date().getTime();
+        var timeout = false;
+        if (now > startTimeMatch + timeRound * 1000) {
+            timeout = true;
+        }
+        return allUserAnswered || timeout;
+    }
+
+    function randomQuestion() {
+        var length = questions.length;
+        var index = Math.floor(Math.random() * length);
+        var random = questions[index];
+        console.log("Random Question: ", random);
+        return random;
+    }
+
+    function sendQuestion() {
+        allUserAnswered = false;
+        for (var i = 0; i < clients.length; i++) {
+            if (i == currentIndexUser) {
+                sendAnswerQuestion(clients[i].socket, currentQuestion);
+            } else {
+                sendQuestionHideAnswer(clients[i].socket, currentQuestion);
+            }
+        }
+    }
+
+    function reGrantDrawPermission(all) {
+        if (all === true) {
+            io.emit("grantDrawPermission", { id: "", all: all });
+        } else {
+            if (clients.length > currentIndexUser) {
+                io.emit("grantDrawPermission", { id: clients[currentIndexUser].id, all: all });
+            }
+        }
+    }
+
+    function sendAnswerQuestion(socket, answer) {
+        socket.emit("answerQuestion", { answer: answer });
+    }
+
+    function sendQuestionHideAnswer(socket, answer) {
+        var text = "";
+        for (var i = 0; i < answer.length; i++) {
+            if (answer[i] == " ") text += " ";
+            else if (answer[i] == "-") text += "-";
+            else text += "=";
+        }
+        socket.emit("answerHide", { answer: text });
+    }
+
+    socket.on("execute", function (data) {
+        if (data.command == "!endgame") {
+            console.log("Execute command End game!");
+            isFinishGame = true;
+        }
+        if (data.command == "!downloaddataset") {
+            console.log("Download Dataset");
+            download("https://gitlab.com/tungxuan1656/drawdoodle/-/raw/master/dataset?inline=false", "dataset.txt", function () {
+                appendNotification("download finish!");
+            });
+        }
+        if (data.command == "!loaddata") {
+            console.log("Download data");
+            loadData(
+                function () {
+                    appendNotification("Load data success");
+                },
+                function () {
+                    appendNotification("Load data error");
+                }
+            );
+        }
+    });
+
+    socket.on("hold-connection", function () {
+        // console.log(socket.id, "hold connection");
+    });
+
+    function loadData(completion, error) {
+        var fs = require("fs"),
+            path = require("path"),
+            filePath = path.join(__dirname, "dataset.txt");
+
+        fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+            if (!err) {
+                questions = data.trim().split("\n");
+                console.log("load data: " + questions);
+                completion();
+            } else {
+                console.log(err);
+                error();
+            }
+        });
+    }
+
+    function download(dataurl, filename, completion) {
+        const http = require("https");
+        const fs = require("fs");
+
+        const file = fs.createWriteStream(filename);
+        const request = http.get(dataurl, function (response) {
+            response.pipe(file);
+            completion();
+        });
+    }
+});
---
+++ public/javascripts/scripts.js
@@ -0,0 +1,318 @@
+(function () {
+    var App = {};
+
+    App.init = function () {
+        App.userName = Math.random().toString(36).substring(2);
+        (function() {
+            var person = prompt("Nhập tên người chơi:");
+            if (person != null && person != "") {
+                App.userName = person;
+            }
+        })();
+        App.point = 0;
+        App.buttonStart = $("#button-start");
+        App.slider = $("#myRange");
+        App.canvas = document.getElementById("canvas");
+        App.canvas.height = 500;
+        App.canvas.width = 700;
+        App.ctx = App.canvas.getContext("2d");
+        App.ctx.fillStyle = "solid";
+        App.ctx.strokeStyle = "#000000";
+        App.ctx.lineWidth = 5;
+        App.ctx.lineCap = "round";
+        App.socket = io.connect('http://localhost:4000');
+        App.isDraw = true;
+        App.timeOutInterval = undefined;
+
+        App.socket.on('connect', function() {
+            App.socket.emit("init", {name: App.userName, point: App.point});
+        });
+
+        App.users = [];
+
+        // App.socket.emit("init", {name: App.userName});
+        
+        App.socket.on("alert", function(data) {
+            alert(data.message);
+        });
+
+        App.socket.on("appendChat", function(data) {
+            App.appendToCmts(App.getElementComment(data.name, data.message));
+        });
+
+        App.socket.on("appendChatCorrectAnswer", function(data) {
+            App.appendToCmts(App.getElementAnswerCorrect(data.name));
+        });
+
+        App.socket.on("appendNotification", function(data) {
+            App.appendToCmts(App.getElementNotification(data.message));
+        });
+
+        App.socket.on("appendNotificationPoint", function(data) {
+            App.appendToCmts(App.getElementNotificationPoint(data.message));
+        });
+
+        App.socket.on("draw", function(data) {
+            return App.draw(data.x, data.y, data.type);
+        });
+
+        App.socket.on("clear_all", function(data) {
+            return App.clear_all();
+        });
+
+        App.socket.on("hideHostControl", function(data){
+            $("#host-control").hide();
+        });
+
+        App.socket.on("changeColor", function(data) {
+            console.log(data.color);
+            App.pickr.setColor(data.color);
+            App.ctx.strokeStyle = data.color;
+        });
+
+        App.socket.on("updateUsers", function(data) {
+            var usersContainer = $("#users-container");
+            usersContainer.empty();
+            data.forEach(user => {
+                usersContainer.append(App.getElementUser(user.name, user.point));
+            });
+        });
+
+        App.socket.on("changeLineWidth", function(data) {
+            console.log(data.lineWidth);
+            App.ctx.lineWidth = data.lineWidth;
+            App.slider.val(data.lineWidth);
+        });
+
+        App.socket.on("changeHost", function(data) {
+            $("#host-control").show();
+        });
+
+        App.socket.on("grantDrawPermission", function(data) {
+            if (data.all === true) {
+                App.isDraw = true;
+                $("#view-control").show();
+                return;
+            }
+            if (data.id === App.socket.id) {
+                $("#view-control").show();
+                App.isDraw = true;
+            }
+            else {
+                $("#view-control").hide();
+                App.isDraw = false;
+            }
+        });
+
+        App.socket.on("answerQuestion", function(data) {
+            console.log(data);
+            $("#answer").empty();
+            $("#answer").append(data.answer);
+        });
+
+        App.socket.on("answerHide", function(data) {
+            console.log(data);
+            $("#answer").empty();
+            $("#answer").append(data.answer);
+        });
+
+        App.socket.on("startMatch", function(data) {
+            // refresh label time-out
+            console.log(data.timeRound);
+            var timeRound = parseInt(data.timeRound);
+            if (timeRound == null || timeRound == undefined) return;
+            clearTimeout(App.timeOutInterval);
+            setLabelTimeout(timeRound);
+        });
+
+        function setLabelTimeout(time) {
+            $("#time-out").empty();
+            $("#time-out").append(time);
+            time -= 1;
+            if (time < 0) return;
+            App.timeOutInterval = setTimeout(function() {
+                setLabelTimeout(time);
+            }, 1000);
+        }
+
+        App.buttonStart.on("click", function() {
+            var round = parseInt($("#round-number").val());
+            if (round == null || round == undefined) round = 1;
+            var timeRound = parseInt($("#time-round").val());
+            if (timeRound == null || timeRound == undefined) timeRound = 120;
+            App.socket.emit("startGame", {round: round, timeRound: timeRound});
+        });
+
+        App.draw = function (x, y, type) {
+            if (type === "dragstart") {
+                App.ctx.beginPath();
+                return App.ctx.moveTo(x, y);
+            } else if (type === "drag") {
+                App.ctx.lineTo(x, y);
+                return App.ctx.stroke();
+            } else {
+                return App.ctx.closePath();
+            }
+        };
+        App.clear_all = function () {
+            App.ctx.clearRect(0, 0, App.canvas.width, App.canvas.height);
+        };
+
+        App.pickr = Pickr.create({
+            el: ".color-picker",
+            theme: "classic", // or 'monolith', or 'nano'
+
+            default: "#000000",
+
+            components: {
+                // Main components
+                preview: true,
+                opacity: false,
+                hue: true,
+
+                // Input / output Options
+                interaction: {
+                    hex: true,
+                    rgba: true,
+                    hsla: true,
+                    hsva: true,
+                    cmyk: true,
+                    input: true,
+                    clear: true,
+                    save: true,
+                },
+            },
+        });
+
+        App.slider.on("input change", function() {
+            if (App.isDraw == true) {
+                App.ctx.lineWidth = App.slider.val();
+                App.socket.emit("changeLineWidth", {lineWidth: App.slider.val()});
+            }
+        });
+
+        App.pickr.on('init', instance => {
+            console.log('init', instance);
+        }).on('hide', instance => {
+            console.log('hide', instance);
+        }).on('show', (color, instance) => {
+            console.log('show', color, instance);
+        }).on('save', (color, instance) => {
+            console.log('save', color, instance);
+            if (App.isDraw == true) {
+                App.ctx.strokeStyle = color.toHEXA();
+                App.socket.emit("changeColor", {color: color.toHEXA().toString()});
+            }
+        }).on('clear', instance => {
+            console.log('clear', instance);
+        }).on('change', (color, instance) => {
+            console.log('change', color, instance);
+        }).on('changestop', instance => {
+            console.log('changestop', instance);
+        }).on('cancel', instance => {
+            console.log('cancel', instance);
+        }).on('swatchselect', (color, instance) => {
+            console.log('swatchselect', color, instance);
+        });
+
+        App.appendToCmts = function(element) {
+            var cmtsArea = $("#cmts");
+            cmtsArea.append(element);
+            cmtsArea.animate({scrollTop: cmtsArea.prop("scrollHeight")});
+        };
+
+        App.getElementUser = function(name, point) {
+            return "<div class=\"user\"><span class=\"user-name\">" + name +"</span><br><span class=\"user-point\">Điểm: " + point + "</span></div>";
+        };
+
+        App.getElementComment = function(name, message) {
+            return "<p class=\"cmt\"><span class=\"user-name\">" + name + ": " + "</span><span class=\"user-comment\">" + message + "</span></p>";
+        };
+
+        App.getElementAnswerCorrect = function(name) {
+            return "<p class=\"cmt\"><span class=\"user-answer-correct\">" + name + " đã trả lời đúng!" + "</span></p>";
+        };
+
+        App.getElementNotification = function(message) {
+            return "<p class=\"cmt notification\">" + message + "</p>";
+        };
+
+        App.getElementNotificationPoint = function(message) {
+            return "<p class=\"cmt notification-point\">" + message + "</p>";
+        };
+
+        window.setInterval(function() {
+            App.socket.emit("hold-connection");
+        }, 10000);
+    };
+    /*
+  	Draw Events
+    */
+    function sendDraw(x, y, type) {
+        if (App.isDraw === false) {
+            return;
+        } 
+        App.socket.emit("drawClick", {
+            x: x,
+            y: y,
+            type: type
+        });
+        App.draw(x,y,type);
+    }
+
+    function handle_mousedown(e){
+        x = e.pageX;
+        y = e.pageY;
+        type = "dragstart";
+        sendDraw(x,y,type);
+        function handle_dragging(e){
+            x = e.pageX;
+            y = e.pageY;
+            type = "drag";
+            sendDraw(x,y,type);
+        }
+        function handle_mouseup(e){
+            $('#canvas')
+            .off('mousemove', handle_dragging)
+            .off('mouseup', handle_mouseup);
+            x = e.pageX;
+            y = e.pageY;
+            type = "dragend";
+            sendDraw(x,y,type);
+            console.log("send dragend");
+        }
+        $('#canvas')
+        .on('mouseup', handle_mouseup)
+        .on('mousemove', handle_dragging);
+    }
+
+    $("#canvas").mousedown(handle_mousedown);
+
+    $("#button-clear-all").on("click", function() {
+        if (App.isDraw == true) {
+            console.log("clear");
+            App.socket.emit("clear_all", {});
+            App.clear_all();
+        }
+    });
+
+    $("#form-comment").submit(function(e) {
+        var text = $("#input").val();
+        if (text == "" && text == null) return;
+
+        $("#input").val("");
+        App.socket.emit("comment", {name: App.userName, text: text});
+        return false;
+    });
+
+    $("#button-execute").on("click", function() {
+        var command = $("#input-command").val();
+        console.log("execute " + command);
+        App.socket.emit("execute", {"command": command});
+    });
+
+    $(function () {
+        console.log("init");
+        return App.init();
+    });
+})(window);
